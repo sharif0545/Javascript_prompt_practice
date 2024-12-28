c01317fb8a8d4cf1e79fbf7957ab6d97f051f1cc
sharif0545 <43298130+sharif0545@users.noreply.github.com>
2024-12-29 01:51:39 +0600

    Added my project

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// email input prompt box
+let validName = "Sharif";
+let validPhone = "01911250545";
+let validPassword = "123";
 
-function emailInput() {
-  let user_text;
+let user_password;
+let user_text;
+let user_phone;
+
+function userInput() {
+  // email input prompt box
+
   let user = prompt("Please enter phone or email:");
   if (user == null || user == "") {
     user_text = "Your email or phone?";
@@ -9,12 +16,9 @@ function emailInput() {
     user_text = user;
   }
   document.getElementById("user").value = user_text;
-}
 
-// password input prompt box
+  // password input prompt box
 
-function passwordInput() {
-  let user_password;
   let password = prompt("Your password:");
   if (password == null || password == "") {
     user_password = "Your password?";
@@ -23,3 +27,17 @@ function passwordInput() {
   }
   document.getElementById("password").value = user_password;
 }
+
+document.getElementById("login").addEventListener("submit", function (e) {
+  e.preventDefault(0);
+  let user_text = document.getElementById("user").value;
+  let user_password = document.getElementById("password").value;
+  if (
+    (user_text === validName || validPhone) &&
+    user_password === validPassword
+  ) {
+    alert("Your are successfully loggedin!");
+  } else {
+    alert("Invalid User details!");
+  }
+});
